5d433eca3738773b87a90b5dd4a46c0c07de88e9
pingukitty <135322848+pingukitty@users.noreply.github.com>
2023-10-07 19:01:11 -0500

    modified:   main/arduino_main.cpp

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -1,18 +1,19 @@
-/****************************************************************************
-Copyright 2021 Ricardo Quesada
+//
+//Copyright 2021 Ricardo Quesada
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+  //  http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-****************************************************************************/
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//***************************************************************************
+//
 
 #include "sdkconfig.h"
 #ifndef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
@@ -28,7 +29,7 @@ limitations under the License.
 
 //
 // README FIRST, README FIRST, README FIRST
-//
+//a
 // Bluepad32 has a built-in interactive console.
 // By default it is enabled (hey, this is a great feature!).
 // But it is incompatible with Arduino "Serial" class.
@@ -81,7 +82,9 @@ void onDisconnectedGamepad(GamepadPtr gp) {
     }
 }
 
-Servo servo;
+Servo servoLeft;
+Servo servoRight;
+
 ESP32SharpIR sensor1( ESP32SharpIR::GP2Y0A21YK0F, 27);
 QTRSensors qtr;
 
@@ -99,12 +102,14 @@ void setup() {
     // But might also fix some connection / re-connection issues.
     BP32.forgetBluetoothKeys();
 
-    ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-    servo.setPeriodHertz(50);
-    servo.attach(12, 1000, 2000);
+    //ESP32PWM::allocateTimer(0);
+	//ESP32PWM::allocateTimer(1);
+	//ESP32PWM::allocateTimer(2);
+	//ESP32PWM::allocateTimer(3);
+    servoLeft.setPeriodHertz(50);
+    servoRight.setPeriodHertz(50);
+    servoLeft.attach(13, 1000, 2000);
+    servoRight.attach(14, 1000, 2000);
 
     // Serial.begin(115200);
     // sensor1.setFilterRate(0.1f);
@@ -135,28 +140,29 @@ void loop() {
 
         if (myGamepad && myGamepad->isConnected()) {
 
-            servo.write( ((((float) myGamepad->axisY()) / 512.0f) * 500) + 1500 );
+            servoLeft.write( ((((float) myGamepad->axisY()) / 512.0f) * 500) + 1500 );
+            servoRight.write( ((((float) myGamepad->axisRY()) / 512.0f) * 500) + 1500 );
 
-            // Another way to query the buttons, is by calling buttons(), or
-            // miscButtons() which return a bitmask.
-            // Some gamepads also have DPAD, axis and more.
-            // Console.printf(
-            //     "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, "
-            //     "%4d, brake: %4d, throttle: %4d, misc: 0x%02x\n",
-            //     i,                        // Gamepad Index
-            //     myGamepad->dpad(),        // DPAD
-            //     myGamepad->buttons(),     // bitmask of pressed buttons
-            //     myGamepad->axisX(),       // (-511 - 512) left X Axis
-            //     myGamepad->axisY(),       // (-511 - 512) left Y axis
-            //     myGamepad->axisRX(),      // (-511 - 512) right X axis
-            //     myGamepad->axisRY(),      // (-511 - 512) right Y axis
-            //     myGamepad->brake(),       // (0 - 1023): brake button
-            //     myGamepad->throttle(),    // (0 - 1023): throttle (AKA gas) button
-            //     myGamepad->miscButtons()  // bitmak of pressed "misc" buttons
-            // );
+            //Another way to query the buttons, is by calling buttons(), or
+            //miscButtons() which return a bitmask.
+           // Some gamepads also have DPAD, axis and more.
+            Console.printf(
+                "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, "
+                "%4d, brake: %4d, throttle: %4d, misc: 0x%02x\n",
+                i,                        // Gamepad Index
+                myGamepad->dpad(),        // DPAD
+                myGamepad->buttons(),     // bitmask of pressed buttons
+                myGamepad->axisX(),       // (-511 - 512) left X Axis
+                myGamepad->axisY(),       // (-511 - 512) left Y axis
+                myGamepad->axisRX(),      // (-511 - 512) right X axis
+                myGamepad->axisRY(),      // (-511 - 512) right Y axis
+                myGamepad->brake(),       // (0 - 1023): brake button
+                myGamepad->throttle(),    // (0 - 1023): throttle (AKA gas) button
+                myGamepad->miscButtons()  // bitmak of pressed "misc" buttons
+            );
 
-            // You can query the axis and other properties as well. See Gamepad.h
-            // For all the available functions.
+           // You can query the axis and other properties as well. See Gamepad.h
+            //For all the available functions.
         }
     }
 
