ab6e6446a531434ede04401b7f1b8440e50f2a4a
pingukitty <135322848+pingukitty@users.noreply.github.com>
2023-10-15 21:03:32 -0500

    modified:   main/arduino_main.cpp

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -1,19 +1,5 @@
-//
-// Copyright 2021 Ricardo Quesada
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//  http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//***************************************************************************
-//
+/* CHECKPOINT FIVE - LINE SENSOR*/
 
 #include "sdkconfig.h"
 #ifndef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
@@ -26,74 +12,14 @@
 #include <ESP32Servo.h>
 #include <ESP32SharpIR.h>
 #include <QTRSensors.h>
-
-//
-// README FIRST, README FIRST, README FIRST
-// a
-// Bluepad32 has a built-in interactive console.
-// By default it is enabled (hey, this is a great feature!).
-// But it is incompatible with Arduino "Serial" class.
-//
-// Instead of using "Serial" you can use Bluepad32 "Console" class instead.
-// It is somewhat similar to Serial but not exactly the same.
-//
-// Should you want to still use "Serial", you have to disable the Bluepad32's console
-// from "sdkconfig.defaults" with:
-//    CONFIG_BLUEPAD32_USB_CONSOLE_ENABLE=n
-
-// GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
-
-// This callback gets called any time a new gamepad is connected.
-// Up to 4 gamepads can be connected at the same time.
-// void onConnectedGamepad(GamepadPtr gp) {
-//     bool foundEmptySlot = false;
-//     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-//         if (myGamepads[i] == nullptr) {
-//             // Console.printf("CALLBACK: Gamepad is connected, index=%d\n", i);
-//             // Additionally, you can get certain gamepad properties like:
-//             // Model, VID, PID, BTAddr, flags, etc.
-//             // GamepadProperties properties = gp->getProperties();
-//             // Console.printf("Gamepad model: %s, VID=0x%04x, PID=0x%04x\n", gp->getModelName(), properties.vendor_id,
-//             //                properties.product_id);
-//             myGamepads[i] = gp;
-//             foundEmptySlot = true;
-//             break;
-//         }
-//     }
-//     if (!foundEmptySlot) {
-//         // Console.println("CALLBACK: Gamepad connected, but could not found empty slot");
-//     }
-// }
-
-// void onDisconnectedGamepad(GamepadPtr gp) {
-//     bool foundGamepad = false;
-
-//     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-//         if (myGamepads[i] == gp) {
-//             // Console.printf("CALLBACK: Gamepad is disconnected from index=%d\n", i);
-//             myGamepads[i] = nullptr;
-//             foundGamepad = true;
-//             break;
-//         }
-//     }
-
-//     if (!foundGamepad) {
-//         // Console.println("CALLBACK: Gamepad disconnected, but not found in myGamepads");
-//     }
-// }
-
 Servo servoLeft;
 Servo servoRight;
 
 // ESP32SharpIR sensor1( ESP32SharpIR::GP2Y0A21YK0F, 27);
 QTRSensors qtr;
 
-// Arduino setup function. Runs in CPU 1
 void setup()
 {
-    // // BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
-    // BP32.forgetBluetoothKeys();
-
     servoLeft.setPeriodHertz(50);
     servoRight.setPeriodHertz(50);
     servoLeft.attach(13, 1000, 2000);
@@ -110,8 +36,6 @@ blue:26:8
 */
     qtr.setTypeAnalog();
     qtr.setSensorPins((const uint8_t[]){36, 39, 34, 35,32,33,25,26}, 8);
-    // and change 3 to 8
-    // qtr.setEmitterPin(2);
 
     Serial.begin(115200);
 
@@ -119,6 +43,7 @@ blue:26:8
     {
         Serial.println("calibrating");
         qtr.calibrate();
+            //Crappy Auto-calibration:
          /*   servoRight.write(2000);
             servoLeft.write(1000);
 
@@ -150,10 +75,6 @@ blue:26:8
     */
 }
 int16_t position = 3500;
-// Arduino loop function. Runs in CPU 1
-
-
-    
 
 void loop()
 {
@@ -235,8 +156,6 @@ else{
     servoRight.write(2000);
     servoLeft.write(2000);
 }
-
-
     delay(10);
 }
 
